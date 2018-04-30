04c79d466ab9d8e62ac60d7f6436b42ac1e6fd23
D7703_06 <d7703_06@d7703-06ui-imac.local>
2018-04-30 14:18:50 +0900

    Initial Commit

---
+++ Simple TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple TableView
+//
+//  Created by D7703_06 on 2018. 4. 30..
+//  Copyright © 2018년 IceArrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
