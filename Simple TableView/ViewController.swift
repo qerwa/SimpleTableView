f2dfc6fcab78b300d61888fa71cab878955354ad
D7703_06 <d7703_06@d7703-06ui-imac.local>
2018-04-30 15:38:27 +0900

    Add table view

--- Simple TableView/ViewController.swift
+++ Simple TableView/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var myTable: UITableView!
+    var data = ["1","2","3","4","5"]
+    var subdata = ["11","22","33","44","55"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        myTable.dataSource = self
+        myTable.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+        //셀 갯수 지정
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let indentifier = "RE"
+        //임시 저장소 셍성
+        let cell = myTable.dequeueReusableCell(withIdentifier: indentifier, for: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
+        cell.detailTextLabel?.text = subdata[indexPath.row]
+        
+        let myImg = UIImage(named:"cat.jpeg")
+        cell.imageView?.image = myImg
+        //animals의 안에 있는 값을 텍스트에 출력
+        return cell
+    }
 
 }
 
